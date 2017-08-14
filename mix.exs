bc7b573cf252e0f96a14d7f9922bd82d998e8c8c
Andrew Harvey <andrew@mootpointer.com>
2017-08-14 13:40:01 +1000

    Fix functions called without parentheses

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule Dolla.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     package: package,
-     description: description
+     deps: deps(),
+     package: package(),
+     description: description()
     ]
   end
 
