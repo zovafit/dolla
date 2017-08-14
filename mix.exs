89d492bc18648fa150dce04ba96f4c53646f6248
Andrew Harvey <andrew@mootpointer.com>
2017-08-14 17:41:01 +1000

    Version up to 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Dolla.Mixfile do
 
   def project do
     [app: :dolla,
-     version: "0.2.3",
-     elixir: "~> 1.2",
+     version: "0.3.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
