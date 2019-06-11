3050d3f83975f6326f9cab7078e4954a7c380898
Jerrold Poh <jerrold.poh@bluecottagesoftware.com>
2019-06-11 10:43:22 +1000

    Add is_in_intro_offer_period field

--- lib/dolla/in_app_receipt.ex
+++ lib/dolla/in_app_receipt.ex
@@ -7,7 +7,7 @@ defmodule Dolla.InAppReceipt do
   defstruct [:quantity, :transaction_id, :original_transaction_id, :purchase_date,
              :original_purchase_date, :expires_date, :cancellation_date, :app_item_id,
              :version_external_identifier, :web_order_line_item_id, :is_trial_period,
-             :product_id
+             :is_in_intro_offer_period, :product_id
             ]
   def decode_template do
     %InAppReceipt{}
